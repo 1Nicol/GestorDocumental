e2cd4246fa9a760ee11d09433e4e65a857b303c2
Juan Benavides Avila <130180526+juanbenavidesavila@users.noreply.github.com>
2024-04-03 20:26:43 -0300

    Update settings.py
    add apps

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-4ju2n@$f9d0c=h)_g0lbb%k9&@rf(xa$d$g$&5ri$uf)*gev^4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".replit.dev", ".replit.app" , "127.0.0.1", "localhost", "127.0.0.1:3000" , "127.0.0.1:8000"]
-CSRF_TRUSTED_ORIGINS = ["https://*.replit.dev", "https://*.replit.app"]
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["*"]
 
 # Application definition
 
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gestor'
+    'django_project.apps.Gestorconfig'
 ]
 
 MIDDLEWARE = [
