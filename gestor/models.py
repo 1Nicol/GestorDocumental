7610dec1f4b54b3cabb9a80bf451fd0b04571313
Nicolle Barrera <156853511+1Nicol@users.noreply.github.com>
2024-04-09 14:41:56 -0500

    Update models.py
    njjijioiio

--- gestor/models.py
+++ gestor/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    contrasena = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Documento(models.Model):
+    nombre_empresa = models.CharField(max_length=100)
+    departamento = models.CharField(max_length=100)
+    sub_departamento = models.CharField(max_length=100)
+    titulo = models.CharField(max_length=255)
+    folio = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.titulo
